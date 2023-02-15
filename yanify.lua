062d0b52747fc3a15a251e7e64ce2588695898b3
poeticjust1ce <simounibarra420@gmail.com>
2023-02-15 19:55:42 +0800

    fixed

--- yanify.lua
+++ yanify.lua
@@ -1,14 +1,3 @@
-repeat task.wait() until game:IsLoaded()
--- config
-getgenv().Config = {
-    Webhook = "https://discord.com/api/webhooks/1075041501081518222/YQfYqa-0QcyElS-5-42fVUrW2txFY78DS8zLrbMxoPJ60OJZ_i994QyP-oAwLZ8yMpel",
-    ServerHop = true,
-    AutoTween = false,
-    IgnoredFruits = {"Spike Fruit"} -- not working atm
-}
-
--- functions
-
 function Notify(fruits)
 
     local date = DateTime.now():FormatLocalTime("MMM/D/YY HH:mm", "en-US")
