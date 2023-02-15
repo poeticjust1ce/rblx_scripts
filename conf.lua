6cc9f14fe592df6c31876717e78b31f06b971f27
poeticjust1ce <simounibarra420@gmail.com>
2023-02-15 20:00:59 +0800

    fixed

--- conf.lua
+++
@@ -1,8 +0,0 @@
-repeat task.wait() until game:IsLoaded()
--- config
-getgenv().Config = {
-    Webhook = "https://discord.com/api/webhooks/1075041501081518222/YQfYqa-0QcyElS-5-42fVUrW2txFY78DS8zLrbMxoPJ60OJZ_i994QyP-oAwLZ8yMpel",
-    ServerHop = true,
-    AutoTween = false,
-    IgnoredFruits = {"Spike Fruit"} -- not working atm
-}
